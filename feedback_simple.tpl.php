f89824dee93b10d86f46fd98a6f21a731061e550
jsongberklee <jsong@berklee.edu>
2016-08-25 11:49:34 -0400

    fa icon added

--- feedback_simple.tpl.php
+++ feedback_simple.tpl.php
@@ -20,6 +20,6 @@
 ?>
 <?php if ($enabled): ?>
 <div id='feedback_simple'>
-  <a href='<?php print $link ?>' target='<?php print $target ?>' class='side-tab-feedback_simple' style='top: 32%;'>ASK LIBRARIAN</a>
+  <a href='<?php print $link ?>' target='<?php print $target ?>' class='side-tab-feedback_simple' style='top: 32%;'>ASK LIBRARIAN<i aria-hidden="true" class="fa fa-commenting" style="margin-right:0;"><span style="display:none;">&nbsp;</span></i></a>
 </div>
 <?php endif;
